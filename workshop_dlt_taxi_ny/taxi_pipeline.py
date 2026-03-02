2d04dab434d63eed99b911caa34133424fdedd01
davbaster <davbaster@gmail.com>
2026-03-02 20:56:56 +0000

    fixed  taxi pipeline

--- workshop_dlt_taxi_ny/taxi_pipeline.py
+++ workshop_dlt_taxi_ny/taxi_pipeline.py
@@ -1,28 +1,19 @@
-"""dlt pipeline for NYC taxi data from the Data Engineering Zoomcamp REST API."""
-
 import dlt
 from dlt.sources.rest_api import rest_api_resources
 from dlt.sources.rest_api.typing import RESTAPIConfig
-
+from dlt.sources.helpers.rest_client.paginators import PageNumberPaginator
 
 BASE_URL = "https://us-central1-dlthub-analytics.cloudfunctions.net/data_engineering_zoomcamp_api"
 
 
 @dlt.source
-def taxi_pipeline_rest_api_source():
-    """Define dlt resources from the NYC taxi REST API (paginated JSON, 1000 records per page). No auth required."""
+def taxi_rest_api_source():
     config: RESTAPIConfig = {
         "client": {
             "base_url": BASE_URL,
         },
         "resource_defaults": {
-            "primary_key": "id",
             "write_disposition": "replace",
-            "endpoint": {
-                "params": {
-                    "limit": 1000,
-                },
-            },
         },
         "resources": [
             {
@@ -33,15 +24,13 @@ def taxi_pipeline_rest_api_source():
                     "params": {
                         "limit": 1000,
                     },
-                    "paginator": {
-                        "type": "offset",
-                        "limit": 1000,
-                        "offset": 0,
-                        "limit_param": "limit",
-                        "offset_param": "offset",
-                        "total_path": None,
-                        "stop_after_empty_page": True,
-                    },
+                    "paginator": PageNumberPaginator(
+                        base_page=1,
+                        page=1,
+                        page_param="page",
+                        stop_after_empty_page=True,
+                        total_path=None,
+                    ),
                 },
             },
         ],
@@ -60,5 +49,5 @@ pipeline = dlt.pipeline(
 
 
 if __name__ == "__main__":
-    load_info = pipeline.run(taxi_pipeline_rest_api_source())
+    load_info = pipeline.run(taxi_rest_api_source())
     print(load_info)
